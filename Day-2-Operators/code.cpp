8a905d78df963f2775dda29697bd7e819ea709a8
anuradha-022 <annumangrati22@gmail.com>
2024-05-02 14:12:58 +0530

    Binary Operator

---
+++ Day-2-Operators/code.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    // const int num = 25;
+    // int num2 = 35;
+    // num2 = 45;
+    //datatype conversion implicitly
+    // cout << (10/3) << endl;
+    // cout << (10/3.0) << endl;
+    // cout << ('A' + 1) << endl;
+    //datatype conversion explicityly
+    // cout << (int)('A') << endl;
+    // cout << ((bool)3 + 2) << endl;
+    // cout << (23.5 + 2 + 'A') << endl;
+    //Arithmatic Operators
+    int a = 5;
+    int b = 3;
+
+    cout << "Addition " <<(a + b) << endl;
+    cout << "Substraction " <<(a - b) << endl;
+    cout << "Multiply " <<(a * b) << endl;
+    cout << "divison " <<(a / b) << endl;
+    cout << "Reminder " <<(a % b) << endl;
+    return 0;
+}
