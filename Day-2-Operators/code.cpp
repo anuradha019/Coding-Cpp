c4a49ca726b10d47aca62cdca4b261c807eb8200
anuradha-022 <annumangrati22@gmail.com>
2024-05-02 21:42:49 +0530

    complete operators till logical operators

--- Day-2-Operators/code.cpp
+++ Day-2-Operators/code.cpp
@@ -5,22 +5,62 @@ int main (){
     // const int num = 25;
     // int num2 = 35;
     // num2 = 45;
+    
     //datatype conversion implicitly
     // cout << (10/3) << endl;
     // cout << (10/3.0) << endl;
     // cout << ('A' + 1) << endl;
+    
     //datatype conversion explicityly
     // cout << (int)('A') << endl;
     // cout << ((bool)3 + 2) << endl;
     // cout << (23.5 + 2 + 'A') << endl;
-    //Arithmatic Operators
-    int a = 5;
-    int b = 3;
+    
+    //Arithmatic Operators 
+    
+    //binary Operator
+    // int a = 5;
+    // int b = 3;
+    // cout << "Addition " <<(a + b) << endl;
+    // cout << "Substraction " <<(a - b) << endl;
+    // cout << "Multiply " <<(a * b) << endl;
+    // cout << "divison " <<(a / b) << endl;
+    // cout << "Reminder " <<(a % b) << endl;
+    
+    //Unary Operator
+    // int a = 3;
+    // a++;
+    // cout << "a = " << a << endl;
+    // a--;
+    // cout << "a = " << a << endl;
 
-    cout << "Addition " <<(a + b) << endl;
-    cout << "Substraction " <<(a - b) << endl;
-    cout << "Multiply " <<(a * b) << endl;
-    cout << "divison " <<(a / b) << endl;
-    cout << "Reminder " <<(a % b) << endl;
+    //Assignment Operator
+    // int a = 10;
+    //  a += 5; // a = a + 5 => 15
+    //  cout << a << endl;
+    //  a -= 5;
+    //  cout << a << endl;
+    //  a *= 5;
+    //  cout << a << endl;
+    //  a /= 5;
+    //  cout << a << endl;
+
+    //Relation Operators -> T / F
+    // int a = 3;
+    // int b = 5;
+    // cout << (a > b) << endl; // false 0
+    // cout << (a < b) << endl; // true 1
+    // cout << (a >= b) << endl; // false 0
+    // cout << (a <= b) << endl; // true 1
+    // cout << (a == b) << endl; // false 0
+    // cout << (a != b) << endl; // true 1
+
+    // logical Operator
+    // cout << ((3 < 5) && (10 > 5 )) << endl;
+    // cout << ((3 < 1) && (10 > 5 )) << endl;
+    // cout << ((3 < 1) || (10 == 5 )) << endl;
+    // cout << (!(3 == 1)) << endl;
+    cout << (!(3 < 1)) << endl;
+
     return 0;
 }
