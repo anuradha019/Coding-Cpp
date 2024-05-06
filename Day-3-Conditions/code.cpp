2fcc190e93f47464b44ba22a783fafe1f6f42bdb
anuradha-022 <annumangrati22@gmail.com>
2024-05-06 21:27:17 +0530

    complete operator switch case

--- Day-3-Conditions/code.cpp
+++ Day-3-Conditions/code.cpp
@@ -38,21 +38,101 @@ using namespace std;
 
 // else if Conditions
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     float marks;
+
+//     cout << "Enter your marks :" << endl;
+//     cin >> marks;
+
+//     if((marks >= 90) && (marks <= 100)) {
+//         cout << "Grade A" << endl;
+//     }  else if ((marks >= 80) && (marks <= 75)){
+//         cout << "Grade B" << endl;
+//     } else {
+//         cout << "Grade C" << endl;
+//     } 
+//     return 0;
+// }
+
+// Ternary Operators ye ek dusra way hai if else codition ko likhne ka in a one line
+ 
+//  #include <iostream>
+//  using namespace std;
+
+//  int main() {
+//     bool isAdult;
+//     int age;
+//     cout << "Enter age : ";
+//     cin >> age;
+
+//     bool isAdult = age >= 18 ? true : false;
+//     cout << "user is " << isAdult << endl;
+//     return 0;
+//  }
+
+//Switch case
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int day = 1;
+//     cout << "Enter day : ";
+//     cin >> day;
+
+//     switch (day)
+//     {
+//     case 1: cout << "Monday" << endl;
+//         break;
+//     case 2: cout << "Tuesday" << endl;
+//         break;
+//     case 3: cout << "Wednesday" << endl;
+//         break;
+//     case 4: cout << "Thursday" << endl;
+//         break;
+//     case 5: cout << "Friday" << endl;
+//         break;
+//     case 6: cout << "Saturday" << endl;
+//         break;
+//     case 7: cout << "Sunday" << endl;
+//         break;
+//     default: cout << "Invalid Day" << endl;
+    
+//     }
+//     return 0;
+// }
+
+// Build a Calculator using operator + - * and /
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    float marks;
+    int a, b;
+    char op;
 
-    cout << "Enter your marks :" << endl;
-    cin >> marks;
+    cout << "enter a : ";
+    cin >> a;
+    cout << "enter b : ";
+    cin >> b;
+    cout << "enter Operators : ";
+    cin >> op;
 
-    if((marks >= 90) && (marks <= 100)) {
-        cout << "Grade A" << endl;
-    }  else if ((marks >= 80) && (marks <= 75)){
-        cout << "Grade B" << endl;
-    } else {
-        cout << "Grade C" << endl;
-    } 
+    switch (op)
+    {
+    case '+': cout << "a + b : " << (a+b) << endl;
+        break;
+    case '-': cout << "a - b : " << (a-b) << endl;
+        break;
+    case '*': cout << "a * b : " << (a*b) << endl;
+        break;
+    case '/': cout << "a / b : " << (a/b) << endl;
+        break;
+    default: cout << "Invalid Operator";
+        break;
+    }
     return 0;
 }
